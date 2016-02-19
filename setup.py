78c0549634bdd073b74bc941eb065b5bf3f3f20f
Alex Elsayed <eternaleye@gmail.com>
2016-02-19 14:36:33 -0800

    Add dependency on hkdf

--- setup.py
+++ setup.py
@@ -85,4 +85,7 @@ setup(name="spake2",
           "Programming Language :: Python :: 3.5",
           "Topic :: Security :: Cryptography",
           ],
+      requires=[
+          'hkdf'
+          ],
       )
