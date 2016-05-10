61c6e825327d5fab669f1c549d9a82e5ec7abf33
Brian Warner <warner@lothar.com>
2016-05-09 22:59:25 -0700

    move that test to test_compat.py

--- src/spake2/test/test_group.py
+++ src/spake2/test/test_group.py
@@ -1,5 +1,5 @@
 import unittest
-from binascii import hexlify, unhexlify
+from binascii import hexlify
 from hashlib import sha256
 from spake2 import groups, params, ed25519_group
 from spake2.spake2 import SPAKE2_A, SPAKE2_B
@@ -165,44 +165,3 @@ class Parameters(unittest.TestCase):
         kA,kB = sA.finish(m1B), sB.finish(m1A)
         self.assertEqual(hexlify(kA), hexlify(kB))
         self.assertEqual(len(kA), len(sha256().digest()))
-
-# These vectors exercise the password-to-scalar conversion step. The vectors
-# should be all JSON, so in the future we can cut-and-paste them into other
-# implementations for compatibility testing.
-# hexlify(b"pw") == "7077"
-# "0001feff" is meant to test non-ASCII passwords
-P2S_TEST_VECTORS = [
-    {"group": "I1024", "pw_hex": "7077",
-     "bytes_hex": "8e0305a470dd82cfd0d3565b26a8cc038a760db1"},
-    {"group": "I1024", "pw_hex": "0001feff",
-     "bytes_hex": "d232b4453ef12bcb5bd1379f8cf5386f8f72c37a"},
-    {"group": "I2048", "pw_hex": "7077",
-     "bytes_hex": "31bfa1a2f261b3d25cb1374659295dc4911970ef2f36b11c298e87b9"},
-    {"group": "I2048", "pw_hex": "0001feff",
-     "bytes_hex": "2e9240620965970def29f7cf8b36553a29459e6136497094a6089bed"},
-    {"group": "I3072", "pw_hex": "7077",
-     "bytes_hex": "03e8e502ef6cd6eeea8d602d55f15b3a843db65a1fb5c6e8875ddc4607d68c8c"},
-    {"group": "I3072", "pw_hex": "0001feff",
-     "bytes_hex": "0689b1f4af52d5baff2d126b7e91d086edc0aceff6721df8bbd29f66dce6ba7d"},
-    {"group": "Ed25519", "pw_hex": "7077",
-     "bytes_hex": "93fefb531c25f73215ed4a6c6c70fedcb2fc653971f1341d4cf1a651c6c6a103"},
-    {"group": "Ed25519", "pw_hex": "0001feff",
-     "bytes_hex": "5a0c70010fb3df10c6ded9b9d57d8088d1676eda41555bb2598bb926a8f67c08"},
-    ]
-P2S_GROUPS = {
-    "I1024": groups.I1024,
-    "I2048": groups.I2048,
-    "I3072": groups.I3072,
-    "Ed25519": ed25519_group.Ed25519Group,
-    }
-
-class PasswordToScalar(unittest.TestCase):
-    def test_vectors(self):
-        for vector in P2S_TEST_VECTORS:
-            group = P2S_GROUPS[vector["group"]]
-            scalar = group.password_to_scalar(unhexlify(vector["pw_hex"]))
-            scalar_bytes = group.scalar_to_bytes(scalar)
-            self.assertEqual(len(scalar_bytes), group.scalar_size_bytes)
-            expected = vector["bytes_hex"].encode("ascii")
-            self.assertEqual(hexlify(scalar_bytes), expected, vector)
-
