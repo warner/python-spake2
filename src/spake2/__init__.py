729de38645caef0334640f09bd316e1edbb6149b
meejah <meejah@meejah.ca>
2024-08-03 14:44:26 -0600

    upgrade versioneer 0.18 -> 0.29

--- src/spake2/__init__.py
+++ src/spake2/__init__.py
@@ -2,6 +2,5 @@
 from .spake2 import SPAKE2_A, SPAKE2_B, SPAKE2_Symmetric, SPAKEError
 SPAKE2_A, SPAKE2_B, SPAKE2_Symmetric, SPAKEError # hush pyflakes
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
